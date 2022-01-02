bbe38784f7565783c5f790fcbfc327d4089cf871
Anwesh43 <anweshthecool0@gmail.com>
2022-01-02 19:10:21 +0530

    withContext

---
+++ src/withContext.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import {useAnimatedScale, useDimension} from './hooks'
+
+
+const withContext = (MainComponent : React.FC<any>) : React.FC<any> => {
+    return () => {
+        const {w, h} = useDimension()
+        const {scale, start: onClick} = useAnimatedScale()
+        const props = {
+            w, 
+            h, 
+            scale, 
+            onClick 
+        }
+        return (
+            <MainComponent {...props}>
+            </MainComponent>
+        )
+    }
+}
+
+export default withContext 
